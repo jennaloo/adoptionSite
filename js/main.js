c7a057110b1129b838588c4a326a1c005cb61b75
Jenna Murphy <jenna.raye.murphy@gmail.com>
2018-10-20 19:14:16 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,84 @@
+var navBar = document.createElement('div');
+navBar.className = "row";
+document.body.appendChild(navBar);
+
+var logoSpace = document.createElement('div');
+logoSpace.className = "col-4 pb-5"
+logoSpace.style.backgroundColor = "#859C0F";
+navBar.appendChild(logoSpace);
+
+var logo = document.createElement('div');
+logo.innerHTML = "<img src='../img/pawPrint.png' width='50px' height='auto'>";
+logo.className = "m-2 pt-3 pl-3";
+logoSpace.appendChild(logo);
+
+var title = document.createElement('div');
+title.className = "col-8 pb-5 pt-3"
+title.innerText = "Up for Adoption";
+title.id = "title";
+title.style.backgroundColor = "#859C0F";
+navBar.appendChild(title);
+
+document.body.style.backgroundColor = "#EDEDED";
+
+function Dog(someArray, name, image, sex, color, breed, age) {
+    this.name = name;
+    this.image = image;
+    this.breed = breed;
+    this.age = age;
+    this.color = color;
+    this.sex = sex;
+    someArray.push(this);
+    this.populateHTML = function () {
+        var container = document.createElement('div');
+        var name = document.createElement('h3');
+        name.innerText = this.name;
+        container.style.backgroundColor = "#EDEDED";
+        name.className = "text-center"
+        container.className = "d-inline-block col-xs-12 col-sm-6 col-md-6 col-xl-4 mt-5 animated fadeInUp";
+        container.id = "container";
+        document.body.appendChild(container);
+        var item = document.createElement('div');
+        item.style.backgroundImage = "url(" + this.image + ")";
+        item.style.backgroundRepeat = "no-repeat";
+        item.style.backgroundSize = "cover";
+        item.id = "item";
+        item.className = "m-2"
+        item.style.border = "1px solid black"
+        container.appendChild(item);
+        container.appendChild(name);
+        var info = document.createElement('div');
+        info.className = "text-center";
+        info.innerText = '' + this.age + ' old ' + this.breed;
+        container.appendChild(info);
+    };
+};
+
+
+var myArray = [];
+
+var Piglet = new Dog(myArray, 'Piglet', '../img/chi.jpg', 'F', 'brown', 'Chihuahua Mix', '5yr');
+
+var Roger = new Dog(myArray, 'Roger', '../img/husky.jpg', 'M', 'White', 'Husky Mix', '6yr');
+
+var Atlas = new Dog(myArray, 'Atlas', '../img/rot.jpg', 'M', 'Black & Brown', 'Rotweiler', '5mo');
+
+var Stormy = new Dog(myArray, 'Stormy', '../img/poodle.jpeg', 'F', 'White', 'Poodle Mix', '2yr');
+
+var Gypsy = new Dog(myArray, 'Gypsy', '../img/pitbull.jpg', 'M', 'Black & White', 'Pitbull', '1yr');
+
+var Emma = new Dog(myArray, 'Emma', '../img/shepard.jpg', 'M', 'Black & Black & Brown', 'Shepard Mix', '2mo');
+
+for (i = 0; i < myArray.length; i++) {
+    console.log(myArray[i].image);
+    myArray[i].populateHTML();
+}
+
+
+var footer = document.createElement('footer');
+footer.className = "col-12 footer";
+footer.style.backgroundImage = "url('../img/grass.jpeg')"
+footer.style.backgroundRepeat = "r";
+footer.style.backgroundSize = "contain";
+footer.id = "footer";
+document.body.appendChild(footer);

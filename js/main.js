5e8867d6e191c1f91bc3c02a43ee53ed558ceb48
Jenna Murphy <jenna.raye.murphy@gmail.com>
2018-11-11 11:29:42 -0800

    added transition

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ navBar.appendChild(logoSpace);
 
 var logo = document.createElement('div');
 logo.innerHTML = "<img src='../img/pawPrint.png' width='50px' height='auto'>";
-logo.className = "m-2 pt-3 pl-3";
+logo.className = "m-2 pt-3 px-0";
 logoSpace.appendChild(logo);
 
 var title = document.createElement('div');
@@ -35,7 +35,7 @@ function Dog(someArray, name, image, sex, color, breed, age) {
         name.innerText = this.name;
         container.style.backgroundColor = "#EDEDED";
         name.className = "text-center"
-        container.className = "d-inline-block col-xs-12 col-sm-6 col-md-6 col-xl-4 mt-5 animated fadeInUp";
+        container.className = "d-inline-block col-xs-12 col-sm-6 col-md-6 col-xl-4 animated fadeInUp m-0";
         container.id = "container";
         document.body.appendChild(container);
         var item = document.createElement('div');
@@ -43,7 +43,7 @@ function Dog(someArray, name, image, sex, color, breed, age) {
         item.style.backgroundRepeat = "no-repeat";
         item.style.backgroundSize = "cover";
         item.id = "item";
-        item.className = "m-2"
+        item.className = "m-0"
         item.style.border = "1px solid black"
         container.appendChild(item);
         container.appendChild(name);
@@ -57,17 +57,17 @@ function Dog(someArray, name, image, sex, color, breed, age) {
 
 var myArray = [];
 
-var Piglet = new Dog(myArray, 'Piglet', '../img/chi.jpg', 'F', 'brown', 'Chihuahua Mix', '5yr');
+var Piglet = new Dog(myArray, 'Piglet', 'img/chi.jpg', 'F', 'brown', 'Chihuahua Mix', '5yr');
 
-var Roger = new Dog(myArray, 'Roger', '../img/husky.jpg', 'M', 'White', 'Husky Mix', '6yr');
+var Roger = new Dog(myArray, 'Roger', 'img/husky.jpg', 'M', 'White', 'Husky Mix', '6yr');
 
-var Atlas = new Dog(myArray, 'Atlas', '../img/rot.jpg', 'M', 'Black & Brown', 'Rotweiler', '5mo');
+var Atlas = new Dog(myArray, 'Atlas', 'img/rot.jpg', 'M', 'Black & Brown', 'Rotweiler', '5mo');
 
-var Stormy = new Dog(myArray, 'Stormy', '../img/poodle.jpeg', 'F', 'White', 'Poodle Mix', '2yr');
+var Stormy = new Dog(myArray, 'Stormy', 'img/poodle.jpeg', 'F', 'White', 'Poodle Mix', '2yr');
 
-var Gypsy = new Dog(myArray, 'Gypsy', '../img/pitbull.jpg', 'M', 'Black & White', 'Pitbull', '1yr');
+var Gypsy = new Dog(myArray, 'Gypsy', 'img/pitbull.jpg', 'M', 'Black & White', 'Pitbull', '1yr');
 
-var Emma = new Dog(myArray, 'Emma', '../img/shepard.jpg', 'M', 'Black & Black & Brown', 'Shepard Mix', '2mo');
+var Emma = new Dog(myArray, 'Emma', 'img/shepard.jpg', 'M', 'Black & Black & Brown', 'Shepard Mix', '2mo');
 
 for (i = 0; i < myArray.length; i++) {
     console.log(myArray[i].image);
@@ -76,9 +76,9 @@ for (i = 0; i < myArray.length; i++) {
 
 
 var footer = document.createElement('footer');
-footer.className = "col-12 footer";
+footer.className = "col-12 footer p-0";
 footer.style.backgroundImage = "url('../img/grass.jpeg')"
-footer.style.backgroundRepeat = "r";
+footer.style.backgroundRepeat = "repeat";
 footer.style.backgroundSize = "contain";
 footer.id = "footer";
 document.body.appendChild(footer);
